07e4f526f4fba93e4cf1229b12b94abe8d2491a5
TomatoTwister <hemantjatal59@gmail.com>
2024-01-06 22:57:26 +0530

    api handler

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import express from "express";
 dotenv.config({
   path: "./env",
 });
+const app = express();
 
-connectDB();
-//const app = express();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`app is listening on port: ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connection failed", err);
+  });
+
 // (async () => {
 //   try {
 //     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
