a2a21ebf27d460ebedafaabd136e32abe8e30380
Codemaverick07 <hemantjatal59@gmail.com>
2024-02-08 23:26:30 +0530

    ytils

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
-connectDB();
+import { app } from "./app.js";
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+  })
+  .catch((error) => {
+    console.log("mongodb connection failed", error);
+  });
 
 // this is used to connect database
 // (async () => {
